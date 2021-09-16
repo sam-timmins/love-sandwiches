95ca14956c16be2b401278b17e1a20b55fa44e33
sam-timmins <sam.timmins1@gmail.com>
2021-09-16 15:03:19 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 
 SCOPE = [
@@ -29,7 +28,7 @@ def get_sales_data():
         print('Please enter sales data from the last market.')
         print('Data should be six numbers, seperated by a commas.')
         print('Example: 10,20,30,40,50,60\n')
-        data_str = input('Enter your data here: ')
+        data_str = input('Enter your data here:\n')
         sales_data = data_str.split(',')
         if validate_data(sales_data):
             print('Data is valid')
